7919a4ed0116e479c237ab909fafd323098ed074
CarterChen <PC@chenyouhongde.local>
2017-09-16 23:04:28 +0800

    the second week

---
+++ the_second_week/wordcount/src/main/java/cn/dataguru/utils/Utils.java
@@ -0,0 +1,24 @@
+package cn.dataguru.utils;
+
+public class Utils {
+	
+	public static void waitForSeconds(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+	public static void waitForMillis(long milliseconds) {
+		try {
+			Thread.sleep(milliseconds);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
